e63df3a177b6fa6b2dd64970dd4a71ba3bed5f49
YafetGetu <kngyafcr@gmail.com>
2024-09-08 00:07:46 -1200

    comment added

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include<fstream>
 using namespace std;
-int key=34;
+int key=34; //you can change the value
 void encrypt( )
 {
     char ch;
-    ifstream fin("E:\\c++\\enc and dec\\orig.txt");
-    ofstream fout("E:\\c++\\enc and dec\\enc.txt");
+    ifstream fin("E:\\c++\\enc and dec\\orig.txt"); //change based on your path(orginal)
+    ofstream fout("E:\\c++\\enc and dec\\enc.txt"); //encrypted file path
     while(!fin.eof()){
         fin.get(ch);
-        ch=ch+key;
+        ch=ch+key;//you can change the opperator
         fout.put(ch);
-
     }
     fin.close();
     fout.close();
